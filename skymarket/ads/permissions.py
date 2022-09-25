1b64bca93bcc2744765fb4f0f41de2baa7720c81
K-Maxim <m.karpenko96@mail.ru>
2022-09-25 18:25:15 +0300

    permission for users

--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -1,1 +1,18 @@
 # TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework.permissions import BasePermission
+from skymarket.users.models import User
+
+
+class UserPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.role == User.ADMIN:
+            return True
+
+        if view.action == 'retrieve':
+            return True
+
+        if view.action in ["create", "update", "partial_update", "destroy"]:
+            return obj.author == request.user or request.user.role == User.ADMIN
