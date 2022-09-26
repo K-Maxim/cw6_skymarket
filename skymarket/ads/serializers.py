e0e5af76dbed373861520c811f33e2db96a0fb3f
K-Maxim <m.karpenko96@mail.ru>
2022-09-26 20:12:50 +0300

    update comment serializer

--- skymarket/ads/serializers.py
+++ skymarket/ads/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 # TODO Сериалайзеры. Предлагаем Вам такую структуру, однако вы вправе использовать свою
 from ads.models import Ad, Comment
+from users.models import User
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -14,23 +15,46 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ("pk", "text", "created_at", "author_id", "ad_id", "author_first_name", "author_last_name")
+        fields = ["pk", "text", "created_at", "author_id", "ad_id", "author_first_name", "author_last_name"]
 
 
 class AdSerializer(serializers.ModelSerializer):
     # TODO сериалайзер для модели
     class Meta:
         model = Ad
-        fields = ("pk", "image", "title", "price", "description")
+        fields = ["pk", "image", "title", "price", "description"]
 
 
 class AdDetailSerializer(serializers.ModelSerializer):
     # TODO сериалайзер для модели
-    author_first_name = serializers.ReadOnlyField(source="author.first_name")
-    author_last_name = serializers.ReadOnlyField(source="author.last_name")
-    phone = serializers.CharField(max_length=20, source="author.phone", read_only=True)
-    author = serializers.ReadOnlyField(source="author.id")
+    phone = serializers.SlugRelatedField(
+        source='author',
+        many=False,
+        queryset=User.objects.all(),
+        slug_field='phone'
+    )
+
+    author_first_name = serializers.SlugRelatedField(
+        source='author',
+        many=False,
+        queryset=User.objects.all(),
+        slug_field='first_name'
+    )
+
+    author_last_name = serializers.SlugRelatedField(
+        source='author',
+        many=False,
+        queryset=User.objects.all(),
+        slug_field='last_name'
+    )
 
     class Meta:
         model = Ad
-        fields = ("pk", "image", "title", "price", "phone", "description", "author_first_name", "author_last_name",  "author_id")
+        fields = ("pk", "image", "title", "price", "phone", "description",
+                  "author_first_name", "author_last_name",  "author_id")
+
+
+class AdCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ad
+        fields = ['image', 'title', 'price', 'description']
