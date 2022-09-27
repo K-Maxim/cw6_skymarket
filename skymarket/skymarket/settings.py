3550710b064e9134877ce7268b9bf040e07df68b
K-Maxim <m.karpenko96@mail.ru>
2022-09-27 21:07:52 +0300

    update djoser

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -97,9 +97,14 @@ REST_FRAMEWORK = {
 # TODO здесь мы настраиваем Djoser
 DJOSER = {
     'SERIALIZERS': {
-        'user_create': 'users.serializers.UserRegistrationSerializer'
+        'user_create': 'users.serializers.UserRegistrationSerializer',
+        "current_user": 'users.serializers.CurrentUserSerializer'
     },
-    'LOGIN_FIELD': 'email'
+    'LOGIN_FIELD': 'email',
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    'EMAIL': {
+        'password_reset': 'users.email.PasswordResetEmail'
+    }
 }
 
 # Database
