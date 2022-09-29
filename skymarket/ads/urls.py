9b2acb9649b44487d5a17a76ea2ac475b6c9414c
K-Maxim <m.karpenko96@mail.ru>
2022-09-29 21:06:25 +0300

    update patch for comments

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -7,10 +7,9 @@ from ads.views import AdViewSet, CommentViewSet
 ads_router = routers.SimpleRouter()
 ads_router.register('ads', AdViewSet)
 
-comments_router = routers.SimpleRouter()
-comments_router.register('comments', CommentViewSet, basename='comments')
+
+ads_router.register('ads/(?P<ad_id>[^/.]+)/comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path("", include(ads_router.urls)),
-    path("", include(comments_router.urls)),
 ]
